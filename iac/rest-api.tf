bf4e8b8086f8387a0c83f0d6937ae7358b0e69f4
EsmerlinJM <esmerlinjm@gmail.com>
2024-05-24 21:35:47 -0300

    [FIX](permission): Fixing lambda permissions

--- iac/rest-api.tf
+++ iac/rest-api.tf
@@ -14,6 +14,8 @@ resource "aws_api_gateway_deployment" "url_shortener_api_deployment" {
       module.post_url_method.integration_id,
       module.redirect_url_method.id,
       module.redirect_url_method.integration_id,
+      module.delete_url_method.id,
+      module.delete_url_method.integration_id,
     ]))
   }
 
@@ -76,7 +78,7 @@ module "delete_url_method" {
   http_method          = "DELETE"
   resource_id          = aws_api_gateway_rest_api.url_shortener_api.root_resource_id
   resource_path        = "/"
-  integration_uri      = module.create_short_url_lambda.invoke_arn
-  lambda_function_name = module.create_short_url_lambda.name
+  integration_uri      = module.delete_lambda.invoke_arn
+  lambda_function_name = module.delete_lambda.name
   execution_arn        = aws_api_gateway_rest_api.url_shortener_api.execution_arn
 }
